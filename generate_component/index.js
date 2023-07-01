af27a33cf016a7fba35ff84df8f1e48bf6e7039f
PeppeTll <peppetll@gmail.com>
2023-07-01 17:30:37 +0200

    fix: validation type

--- generate_component/index.js
+++ generate_component/index.js
@@ -3,17 +3,26 @@ import { index, component, barrel } from './component_templates.js';
 import { index as indexTSX, component as componentTSX, barrel as barrelTSX } from './component_templates_ts.js';
 
 const [name, fileType = 'jsx', indexType = 'css'] = process.argv.slice(2);
-if (!name) throw new Error('You must include a component name.');
+if (!name) {
+  console.error('You must include a component name.');
+  process.exit(1);
+}
 
 const validFileTypes = ['', 'jsx', 'tsx'];
 if (!validFileTypes.includes(fileType)) {
   console.error(`Invalid file type "${fileType}". The file type must be empty, "jsx", or "tsx".`);
+  console.log('Example: npm run gc MyComponent jsx');
+  console.log('Example: npm run gc MyComponent tsx');
   process.exit(1);
 }
 
 const validIndexTypes = ['', 'css', 'scss'];
 if (!validIndexTypes.includes(indexType)) {
   console.error(`Invalid index type "${indexType}". The index type must be empty, "css", or "scss".`);
+  console.log('Example: npm run gc MyComponent jsx css');
+  console.log('Example: npm run gc MyComponent jsx scss');
+  console.log('Example: npm run gc MyComponent tsx css');
+  console.log('Example: npm run gc MyComponent tsx scss');
   process.exit(1);
 }
 
@@ -28,7 +37,7 @@ if (!existsSync(componentsDir)) {
 
 // Throw an error if the component directory already exists
 if (existsSync(dir)) {
-  console.log(`A component with the name "${name}" already exists.`);
+  console.error(`A component with the name "${name}" already exists.`);
   throw new Error('A component with that name already exists.');
 }
 
